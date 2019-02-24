800ee5757e292185398811135e6c0db3531e299f
Scrappy <jedediah.shine100@gmail.com>
2019-02-24 22:34:16 +0300

    start circle

---
+++ canvas/task2/task2.js
@@ -0,0 +1,11 @@
+var canvas = document.getElementById('triangle');
+var context = canvas.getContext('2d');
+
+context.beginPath();
+context.arc(100, 100, 75, 0, degreesToRadians(270), true);
+context.closePath();
+
+context.lineWidth = 5;
+context.stroke();
+context.fillStyle = "red";
+context.fill();
